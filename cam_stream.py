55a5e5c5f357e83438a5af650d27cfe2fabbaf8c
Aymen Hafeez <49293546+aymenhafeez@users.noreply.github.com>
2019-09-01 03:09:46 +0100

    Update cam_stream.py

--- cam_stream.py
+++ cam_stream.py
@@ -7,13 +7,9 @@ cap = cv2.VideoCapture(0)
 while True:
     _, frame = cap.read()
 
-    # Convert to grayscale
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
     cv2.imshow('Frame', frame)
 
     key = cv2.waitKey(1)
-
     if key == 10:
         break
 
